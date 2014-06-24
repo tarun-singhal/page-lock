a6b6b1b6c33b267ab8157564246a90c6341444a5
root <root@administrator.(none)>
2014-06-24 18:46:18 +0530

    view layer addition

--- config/module.config.php
+++ config/module.config.php
@@ -2,5 +2,10 @@
 return array(
 	'PageLock' => array(
         	'PAGE_LOCK' => '10', //in seconds
-        ),	
+        ),
+	'view_manager' => array(
+    		'template_map' => array(
+    			'page-blocked' => __DIR__ . '/../view/page-blocked.phtml',
+    		)
+        )	
 );
